392739f939a98ddfd86823d4980f9897ba7faec0
PengZhong <pengmany@outlook.com>
2016-11-28 15:24:01 +0800

    add config files

---
+++ conf/config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# APS-DATA metadata absolute path
+base_path_posix = r"/media/zhongpeng/Datas/APS-DATA/aps-dataset-metadata-2013"
+base_path_nt = r"E:/APS-DATA/aps-dataset-metadata-2013"
