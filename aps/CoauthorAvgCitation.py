33ddf898b4ec3c3afd74841d164386bcfd2ec393
PengZhong <pengmany@outlook.com>
2016-12-11 13:43:34 +0800

    temp saving

--- aps/CoauthorAvgCitation.py
+++ aps/CoauthorAvgCitation.py
@@ -40,7 +40,7 @@ def get_coauthor_list(author):
     @output: author's all coauthors from the academic start year to the start+time year
     """
     paper_li = get_paper_li_by_author_name(author)
-    print paper_li
+    # print paper_li
     coauthor_set = set()
     for paper in paper_li:
         coauthor_set.update(process.get_author_list_by_doi(base_path, paper))
@@ -48,13 +48,28 @@ def get_coauthor_list(author):
     return list(coauthor_set)
 
 
-def get_all_coauthor's paper_list(author):
+def get_all_coauthor_paper_list(author, author_paper_file_path, year):
     """
     @input params:
     author: author's name
+    author_paper_file_path: the absolute path of author-paper csv file
+    year: year
+    description: get all coauthor's paper list by the end of the parameter year.
     @output: author's all coatuthor's doi list
     """
-    pass
+    coauthor_list = get_coauthor_list(author)
+    all_paper_list = list()
+    with open(author_paper_file_path, "rb") as csvfile:
+        reader = csv.reader(csvfile, dialect="excel")
+        for row in reader:
+            if row[0] in coauthor_list:
+                paper_li = row[1][2: -2].split('\', \'')
+                for paper in paper_li:
+                    if int(process.get_paper_year_by_doi(paper)) <= year:
+                        all_paper_list.append(paper)
+                coauthor_list.remove(row[0])
+                break
+    return all_paper_list
 
 
 def get_coauthor_avg_citation(author, start_year, year=4):
@@ -70,5 +85,5 @@ def get_coauthor_avg_citation(author, start_year, year=4):
 
 if __name__ == '__main__':
     li = get_coauthor_list("Charanjit S. Aulakh")
-    print li
-    print len(li)
+    path = r"../author_all_paper_li_all.csv"
+    print get_all_coauthor_paper_list("Charanjit S. Aulakh", path, 2011)
