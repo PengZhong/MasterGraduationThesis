5d0bf0528ba5428800fef843a996e5d45cd4680f
PengZhong <pengmany@outlook.com>
2016-12-17 22:15:08 +0800

    add function of get_page_rank_dict_by_year and modify the definition and implementation of function create_coauthor_network

--- aps/CoauthorNetwork.py
+++ aps/CoauthorNetwork.py
@@ -13,6 +13,7 @@ sys.path.append("..")
 from conf import config
 import process
 from collections import defaultdict
+from RankAlgorithm import pagerank
 
 
 # get metadata's absolute base path
@@ -42,34 +43,33 @@ def get_all_related_paper_by_year(year, author_paper_file_path):
     return related_paper_list
 
 
-# def get_edge_weight(authorA, authorB, author_paper_dict, paper_rank_dict):
-#     """
-#     calculate authorA's and authorB's edge weight(attr_dict)
-#     @type authorA: string value
-#     @param authorA: authorA's name
-#     @type authorB: string value
-#     @param authorB: authorB's name
-#     @type author_paper_dict: dictionary
-#     @param author_paper_dict: author-paper's dictionary, key is author, value is paper list(string type)
-#     @type paper_rank_dict: dictionary
-#     @param paper_rank_dict: paper's score in citation netowrk
-#     return type: numerical value
-#     return value: the edge weight value between the node authorA and authorB
-#     """
-#     paper_listA = author_paper_dict[authorA]
-#     paper_listB = author_paper_dict[authorB]
-#     paper_setA = set(paper_listA)
-#     paper_setB = set(paper_listB)
-#     coauthor_paper_set = paper_setA & paper_setB
-#     for paper in coauthor_paper_set:
-#         author_list = process.get_author_list_by_doi(paper)
-#         paper_value = paper_rank_dict[paper]
-#         orA = author_list.index(authorA)
-#         orB = author_list.index(authorB)
-#         pass
-#     return edge_weight
+def get_paper_rank_dict_by_year(year, related_paper_list, citation_file_path):
+    """
+    get paper's rank score in the citation netowrk of the year
+    @type year: int value
+    @param year: we use the data of the year to create coauthor network
+    @type related_paper_list: string value
+    @param related_paper_list: all related paper's doi list of the year
+    @type citation_file_path: string value
+    @param citation_file_path: paper citation relation file(aps_full_info_citation.csv)
+    return type: dictionary object
+    return value: a dictionary of paper's doi and rank score's key-value pairs of the year
+    """
+    rank_score_dict = dict()
+    graph = nx.DiGraph()
+    graph.add_nodes_from(related_paper_list)
 
-def create_coauthor_network(year, author_paper_file_path, paper_rank_dict):
+    with open(citation_file_path, "rb") as csvfile:
+        reader = csv.reader(csvfile, dialect="excel")
+        for row in reader:
+            if int(process.get_paper_year_by_doi(row[0])) <= year:
+                if int(process.get_paper_year_by_doi(row[1])) <= year:
+                    graph.add_edge(row[0], row[1])
+    rank_score_dict = basic_pagerank_directed(graph)
+    return rank_score_dict
+
+
+def create_coauthor_network(year, author_paper_file_path, citation_file_path):
 # def create_coauthor_network(author, year, author_paper_file_path, paper_rank_dict):
     """
     create coauthor network of the author in the year.
@@ -79,8 +79,10 @@ def create_coauthor_network(year, author_paper_file_path, paper_rank_dict):
     @param year: we use the data of the year to create coauthor network
     @type author_paper_file_path:string value
     @param author_paper_file_path: author-paper(author_all_paper_li_all.csv) csvfile's absolute path
-    @type paper_rank_dict: dictionary
-    @param paper_rank_dict: paper's score in citation netowrk
+    # @type paper_rank_dict: dictionary
+    # @param paper_rank_dict: paper's score in citation netowrk
+    @type citation_file_path: string value
+    @param citation_file_path: paper citation relation file(aps_full_info_citation.csv)
     return type: a networkx object of directed network(two direction link between two nodes)
     return value: the coauthor network
     """
@@ -88,6 +90,7 @@ def create_coauthor_network(year, author_paper_file_path, paper_rank_dict):
     graph = nx.DiGraph()
     # related_paper_list = get_all_related_paper_by_author(author, year, author_paper_file_path)
     related_paper_list = get_all_related_paper_by_year(year, author_paper_file_path)
+    paper_rank_dict = get_paper_rank_dict_by_year(year, related_paper_list, citation_file_path)
     for related_paper in related_paper_list:
         author_list = process.get_author_list_by_doi(base_path, related_paper)
         length = len(author_list)
